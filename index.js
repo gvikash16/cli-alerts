64f9a98864887cac81151d1048b6d002c947fda9
Vikash <vikash.lnkin@gmail.com>
2022-12-10 07:54:08 +0530

    added alert for error

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import alert from './utils/alert.js';
-// alert();
-
+const flags = process.argv.slice(2)[0];
+const input = process.argv.slice(2)[1];
+if(flags || input) {
+  console.log('flags', flags);
+  console.log('input', input);
+} else{
+  alert({type: 'error', msg: 'You have not entered any flag or input'});
+}
